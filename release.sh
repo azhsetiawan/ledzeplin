bd18c55dded843dc188f669602681832d1fa66b4
azhsetiawan <azhsetiawan@users.noreply.github.com>
2020-10-01 23:09:01 +0700

    rename completion file

--- release.sh
+++ release.sh
@@ -3,7 +3,6 @@
 mkdir -p ./release/completion
 cp ./bin/ledzeplin ./release/ledzeplin
 osacompile -x -o ./release/blackdog.scpt ./bin/blackdog.applescript
-cp ./completion/*.zsh ./release/completion/_ledzeplin
-cp ./completion/*.bash ./release/completion/ledzeplin
+cp ./completion/* ./release/completion/
 
 tar -czvf ledzeplin.tar.gz ./release && rm -rf ./release
